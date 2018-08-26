ca13fa71786bf5627d1555a2df919b3ef851463e
justin.cs.kim@gmail.com <justin.cs.kim@gmail.com>
2018-08-26 00:03:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element) {
-  return[...array, element];
-}
-
-function addElementToBeginningOfArray (array, element) {
   return[element, ...array];
 }
 
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  return[element, ...array];
+}
+
+var destructivelyAddElementToBeginningOfArray = [array, element];
+  
+  destructivelyAddElementToBeginningOfArray.unshift(array, element);
